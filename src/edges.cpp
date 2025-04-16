537db61897316db82a9bded79c1222c2d194b93d
Phyoenix <tangshenghao2@gmail.com>
2025-04-16 19:40:38 +0800

    comp[lete tasks

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,28 @@
 #include "edges.h"
+#include <set>
+#include <vector>
+#include <algorithm>
 
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
-  Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+  std::set<std::pair<int, int>> edge_set;
+  
+  for (int i = 0; i < F.rows(); ++i) {
+    int v0 = F(i, 0);
+    int v1 = F(i, 1);
+    int v2 = F(i, 2);
+    edge_set.insert(std::minmax(v0, v1));
+    edge_set.insert(std::minmax(v1, v2));
+    edge_set.insert(std::minmax(v2, v0));
+  }
+  Eigen::MatrixXi E(edge_set.size(), 2);
+  int idx = 0;
+  for (const auto &edge : edge_set) {
+    E(idx, 0) = edge.first;
+    E(idx, 1) = edge.second;
+    ++idx;
+  }
+  
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,32 @@
 #include "euler_characteristic.h"
+#include <set>
+#include <algorithm>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
-  int Chi = 0;
   // ADD YOUR CODE HERE
+  // record the number of vertices
+  std::set<int> unique_vertices;
+  for (int i = 0; i < F.rows(); ++i) {
+    unique_vertices.insert(F(i, 0));
+    unique_vertices.insert(F(i, 1));
+    unique_vertices.insert(F(i, 2));
+  }
+  // record the number of edges
+  std::set<std::pair<int, int>> unique_edges;
+  for (int i = 0; i < F.rows(); ++i) {
+    int v0 = F(i, 0);
+    int v1 = F(i, 1);
+    int v2 = F(i, 2);
+    unique_edges.insert(std::minmax(v0, v1));
+    unique_edges.insert(std::minmax(v1, v2));
+    unique_edges.insert(std::minmax(v2, v0));
+  }
+
+  int V = unique_vertices.size();
+  int E = unique_edges.size();
+  int num_faces = F.rows();
+  auto Chi = V - E + num_faces;
+  
   return Chi;
 }
